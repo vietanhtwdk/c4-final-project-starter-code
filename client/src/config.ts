510dc63c301f89e5059908f1410202e643bcf759
vietanhtwdk <vietanhtruongwdk@gmail.com>
2022-07-21 20:14:23 +0700

    update model, remove todo

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'cq7ohpftme'
+const apiId = 'yf0ph07yq6'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
